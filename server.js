9e4df26822b0b6a6c9f381d056eec3da89026b3f
Aidan Walbran <75866732+NeWaffless@users.noreply.github.com>
2025-01-27 22:13:53 +1100

    app.use(cors) is more specific

--- server.js
+++ server.js
@@ -12,7 +12,18 @@ import {
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 app.use(express.json());
 
 app.get("/", (req, res) => {
